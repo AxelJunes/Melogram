f5c97d77767ebd564bdf238429ff798f8d7c952c
AxelJunes <axel.junestrand@hotmail.com>
2017-05-28 16:16:34 +0200

    Mobile responsive

--- app/views/layout/library.php
+++ app/views/layout/library.php
@@ -1,4 +1,5 @@
 <?php echo "<!-- IMPORT RESOURCE HTML -->" ?>
+<meta name="viewport" content="width=device-width, initial-scale=1">
 <meta charset="UTF-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1">
